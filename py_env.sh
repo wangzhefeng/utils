24528c551311334c8badca2d0e20c838e63b8c0a
wangzhefeng <wangzhefengr@163.com>
2025-03-31 00:20:58 +0800

    update

---
+++ py_env.sh
@@ -0,0 +1,8 @@
+pip install tqdm
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install scikit-learn
+pip install jupyter
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
