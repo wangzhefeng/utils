e15eb241691bbeafdfa01665bd2897a9f9ea5298
wangzhefeng <wangzhefengr@163.com>
2025-05-06 00:18:27 +0800

    update

---
+++ exp/exp_template.py
@@ -0,0 +1,337 @@
+# -*- coding: utf-8 -*-
+
+# ***************************************************
+# * File        : exp_template.py
+# * Author      : Zhefeng Wang
+# * Email       : zfwang7@gmail.com
+# * Date        : 2025-01-13
+# * Version     : 1.0.011321
+# * Description : description
+# * Link        : link
+# * Requirement : 相关模块版本需求(例如: numpy >= 2.1.0)
+# * TODO        : 1.
+# ***************************************************
+
+# python libraries
+import os
+import sys
+ROOT = os.getcwd()
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+import time
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+from exp.exp_basic import Exp_Basic
+# data pipeline
+from data_provider.data_factory import data_provider
+# model training
+from utils.model_tools import adjust_learning_rate, EarlyStopping
+# loss
+# from utils.losses import mape_loss, mase_loss, smape_loss
+from utils.plot_losses import plot_losses
+# log
+from utils.timestamp_utils import from_unix_time
+from utils.log_util import logger
+
+plt.rcParams['font.sans-serif']=['SimHei']    # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False    # 用来显示负号
+
+# global variable
+LOGGING_LABEL = __file__.split('/')[-1][:-3]
+
+
+class Model(Exp_Basic):
+
+    def __init__(self, args):
+        logger.info(f"{40 * '-'}")
+        logger.info("Initializing Experiment...")
+        logger.info(f"{40 * '-'}")
+        super(Model, self).__init__(args)
+
+    def _build_model(self):
+        """
+        模型构建
+        """
+        # 模型初始化
+        logger.info(f"Initializing model {self.args.model}...")
+        model = self.model_dict[self.args.model].Model(self.args)
+        # 多 GPU 训练
+        if self.args.use_gpu and self.args.use_multi_gpu:
+            model = nn.DataParallel(model, device_ids = self.args.devices)
+        # 打印模型参数量
+        total = sum([param.nelement() for param in model.parameters()])
+        logger.info(f'Number of model parameters: {(total / 1e6):.2f}M')
+        
+        return model
+    
+    def _get_data(self, flag: str):
+        """
+        数据集构建
+        """
+        data_set, data_loader = data_provider(self.args, flag)
+        
+        return data_set, data_loader
+    
+    def _select_criterion(self):
+        """
+        评价指标
+        """
+        pass
+
+    def _select_optimizer(self):
+        """
+        优化器
+        """
+        optimizer = torch.optim.Adam(self.model.parameters(), lr = self.args.learning_rate)
+        
+        return optimizer
+    
+    def _get_model_path(self, setting):
+        """
+        模型保存路径
+        """
+        # 模型保存路径
+        model_path = os.path.join(self.args.checkpoints, setting)
+        os.makedirs(model_path, exist_ok=True)
+        # 最优模型保存路径
+        model_checkpoint_path = os.path.join(model_path, "checkpoint.pth")
+        
+        return model_checkpoint_path
+    
+    def _get_test_results_path(self, setting):
+        """
+        结果保存路径
+        """
+        results_path = os.path.join(self.args.test_results, setting)
+        os.makedirs(results_path, exist_ok=True)
+        
+        return results_path
+    
+    def _model_forward(self):
+        return None
+   
+    def train(self, setting):
+        """
+        模型训练
+        """
+        # 数据集构建
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        # checkpoint 保存路径
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Model checkpoint will be saved in path:")
+        logger.info(f"{40 * '-'}")
+        model_checkpoint_path = self._get_model_path(setting)
+        logger.info(model_checkpoint_path)
+        # 测试结果保存地址
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Train results will be saved in path:")
+        logger.info(f"{40 * '-'}")
+        test_results_path = self._get_test_results_path(setting) 
+        logger.info(test_results_path)
+        # 模型训练
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Model start training...")
+        logger.info(f"{40 * '-'}")
+        # time: 模型训练开始时间
+        train_start_time = time.time()
+        logger.info(f"Train start time: {from_unix_time(train_start_time).strftime('%Y-%m-%d %H:%M:%S')}")
+        # 训练窗口数
+        train_steps = len(train_loader)
+        logger.info(f"Train steps: {train_steps}") 
+        # 模型优化器
+        optimizer = self._select_optimizer()
+        logger.info(f"Train optimizer has builded...")
+        # 模型损失函数
+        criterion = self._select_criterion()
+        logger.info(f"Train criterion has builded...")
+        # 早停类实例
+        early_stopping = EarlyStopping(patience = self.args.patience, verbose = True)
+        logger.info(f"Train early stopping instance has builded, patience: {self.args.patience}")
+        # 自动混合精度训练
+        if self.args.use_amp:
+            scaler = torch.amp.GradScaler()
+        # 训练、验证结果收集
+        train_losses, vali_losses = [], []
+        # 分 epoch 训练
+        for epoch in range(self.args.train_epochs):
+            # time: epoch 训练开始时间
+            epoch_start_time = time.time()
+            logger.info(f"Epoch: {epoch+1} \tstart time: {from_unix_time(epoch_start_time).strftime('%Y-%m-%d %H:%M:%S')}")
+            # epoch 训练结果收集
+            iter_count = 0
+            train_loss = []
+            # 模型训练模式
+            self.model.train()
+            for i, (batch_x, batch_y) in enumerate(train_loader):
+                # 当前 epoch 的迭代次数记录
+                iter_count += 1
+                # 模型优化器梯度归零
+                optimizer.zero_grad()
+                # 前向传播
+                outputs = self._model_forward(batch_x)
+                # 计算训练损失
+                loss = criterion(outputs, batch_y)
+                train_loss.append(loss.item())
+                # 当前 epoch-batch 下每 100 个 batch 的训练速度、误差损失
+                if (i + 1) % 10 == 0:
+                    speed = (time.time() - train_start_time) / iter_count
+                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                    logger.info(f'Epoch: {epoch + 1}, \tBatch: {i + 1} | train loss: {loss.item():.7f}, \tSpeed: {speed:.4f}s/batch; left time: {left_time:.4f}s')
+                    iter_count = 0
+                    train_start_time = time.time()
+                # 后向传播、参数优化更新
+                if self.args.use_amp:
+                    scaler.scale(loss).backward()
+                    scaler.step(optimizer)
+                    scaler.update()
+                else:
+                    loss.backward()
+                    optimizer.step()
+            logger.info(f"Epoch: {epoch + 1}, \tCost time: {time.time() - epoch_start_time}")
+            # 模型验证
+            train_loss = np.average(train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion, test_results_path)
+            logger.info(f"Epoch: {epoch + 1}, Steps: {train_steps} | Train Loss: {train_loss:.7f}, Vali Loss: {vali_loss:.7f}")
+            # 训练/验证损失收集
+            train_losses.append(train_loss)
+            vali_losses.append(vali_loss)
+            # 早停机制、模型保存
+            early_stopping(
+                vali_loss, 
+                epoch=epoch, 
+                model=self.model, 
+                optimizer=optimizer, 
+                scheduler=None, 
+                model_path=model_checkpoint_path,
+            )
+            if early_stopping.early_stop:
+                logger.info(f"Epoch: {epoch + 1}, \tEarly stopping...")
+                break
+            # 学习率调整
+            adjust_learning_rate(optimizer, epoch + 1, self.args)
+        # -----------------------------
+        # 模型加载
+        # ------------------------------
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Training Finished!")
+        logger.info(f"{40 * '-'}")
+        # plot losses
+        logger.info("Plot and save train/vali losses...")
+        plot_losses(
+            train_epochs=self.args.train_epochs,
+            train_losses=train_losses, 
+            vali_losses=vali_losses, 
+            label="loss",
+            results_path=test_results_path
+        )
+        # load model
+        logger.info("Loading best model...")
+        self.model.load_state_dict(torch.load(model_checkpoint_path)["model"])
+        # return model and train results
+        logger.info("Return training results...")
+        return self.model
+
+    def vali(self, vali_loader, criterion):
+        """
+        模型验证
+        """
+        # 模型开始验证
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Model start validating...")
+        logger.info(f"{40 * '-'}")
+        # 验证窗口数
+        vali_steps = len(vali_loader)
+        logger.info(f"Vali steps: {vali_steps}")
+        # 模型验证结果
+        vali_loss = []
+        # 模型评估模式
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y) in enumerate(vali_loader):
+                # 前向传播
+                outputs = self._model_forward(batch_x)
+                # 计算/保存验证损失
+                loss = criterion(outputs, batch_y)
+                vali_loss.append(loss)
+        # 计算验证集上所有 batch 的平均验证损失
+        vali_loss = np.average(vali_loss)
+        # 计算模型输出
+        self.model.train()
+        # log
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Validating Finished!")
+        logger.info(f"{40 * '-'}")
+        
+        return vali_loss
+
+    def test(self, flag, setting, load: bool=False):
+        """
+        模型测试
+        """
+        # 数据集构建
+        test_data, test_loader = self._get_data(flag=flag) 
+        # 模型加载
+        if load:
+            logger.info(f"{40 * '-'}")
+            logger.info("Pretrained model has loaded from:")
+            logger.info(f"{40 * '-'}")
+            model_checkpoint_path = self._get_model_path(setting)
+            self.model.load_state_dict(torch.load(model_checkpoint_path)["model"]) 
+            logger.info(model_checkpoint_path)
+        # 测试结果保存地址
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Test results will be saved in path:")
+        logger.info(f"{40 * '-'}")
+        test_results_path = self._get_test_results_path(setting) 
+        logger.info(test_results_path) 
+        # 模型开始测试
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Model start testing...")
+        logger.info(f"{40 * '-'}")
+        # 模型测试次数
+        test_steps = len(test_loader)
+        logger.info(f"Test steps: {test_steps}")
+        # 模型评估模式
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y) in enumerate(test_loader):
+                logger.info(f"test step: {i}")
+                # 前向传播
+                outputs = self._model_forward(batch_x)
+        # 测试结果保存
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Test metric results have been saved in path:")
+        logger.info(f"{40 * '-'}")
+        logger.info(test_results_path)
+        # 测试结果可视化
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Test visual results have been saved in path:")
+        logger.info(f"{40 * '-'}")
+        logger.info(test_results_path)
+        # log
+        logger.info(f"{40 * '-'}")
+        logger.info(f"Testing Finished!")
+        logger.info(f"{40 * '-'}")
+
+        return
+
+    def inference(self, setting, load: bool=True):
+        """
+        模型预测
+        """
+        pass
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ exp/run_template.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+# ***************************************************
+# * File        : run.py
+# * Author      : Zhefeng Wang
+# * Email       : zfwang7@gmail.com
+# * Date        : 2025-01-08
+# * Version     : 1.0.010821
+# * Description : description
+# * Link        : link
+# * Requirement : 相关模块版本需求(例如: numpy >= 2.1.0)
+# * TODO        : 1.
+# ***************************************************
+
+__all__ = []
+
+# python libraries
+import os
+import sys
+ROOT = os.getcwd()
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+import argparse
+
+from exp.exp_forecasting import Exp_Forecast
+from utils.print_args import print_args
+from utils.device import torch_gc
+from utils.random_seed import set_seed
+from utils.log_util import logger
+
+# global variable
+LOGGING_LABEL = __file__.split('/')[-1][:-3]
+
+
+def args_parse():
+    parser = argparse.ArgumentParser(description='LLM')
+    # basic config
+    parser.add_argument('--task_name', type=str, required=True, default='pretrain', help='task name')
+    parser.add_argument('--is_training', type=int, required=True, default=0, help='Whether to conduct training')
+    parser.add_argument('--is_testing', type=int, required=True, default=0, help='Whether to conduct testing')
+    parser.add_argument('--is_inference', type=int, required=True, default=0, help='Whether to conduct inference')
+    parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
+    parser.add_argument('--model', type=str, required=True, default='Transformer', help='model name')
+    # data loader
+    parser.add_argument('--root_path', type=str, required=True, default='./dataset/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, required=True, default='ETTh1.csv', help='data file')
+    parser.add_argument('--data', type=str, required=True, default='ETTh1', help='dataset type')
+    parser.add_argument('--checkpoints', type=str, default='./saved_results/pretrained_models/', help='location of model models')
+    parser.add_argument('--test_results', type=str, default='./saved_results/test_results/', help='location of model models')
+    parser.add_argument('--predict_results', type=str, default='./saved_results/predict_results/', help='location of model models') 
+    # forecasting task
+    parser.add_argument('--train_ratio', type=float, required=True, default=0.7, help='train dataset ratio')
+    parser.add_argument('--test_ratio', type=float, required=True, default=0.2, help='test dataset ratio')
+    # model define
+    parser.add_argument('--top_k', type=int, default=5, help='for TimesBlock')
+    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+    parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+    parser.add_argument('--iters', type=int, default=1, help='train iters')
+    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--patience', type = int, default=3, help = 'early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=1e-4, help='optimizer learning rate')
+    parser.add_argument('--loss', type=str, default='mse', help='loss function')
+    parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+    parser.add_argument('--use_amp', type=int, default=0, help='use automatic mixed precision training') 
+    # GPU
+    parser.add_argument('--use_gpu', type=int, default=1, help='use gpu')
+    parser.add_argument('--gpu_type', type=str, default='cuda', help='gpu type')
+    parser.add_argument('--use_multi_gpu', type=int, default=0, help = 'use multiple gpus')
+    parser.add_argument('--devices', type=str, default="0,1,2,3,4,5,6,7,8", help='device ids of multile gpus')
+    
+    # 命令行参数解析
+    args = parser.parse_args()
+
+    return args
+
+
+def run(args):
+    # setting record of experiments
+    setting = '{}_{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_expand{}_dc{}_fc{}_eb{}_dt{}_{}_te{}_{}_{}_'.format(
+        args.task_name,
+        args.model_id,
+        args.model,
+        args.data,
+        args.features,
+        args.seq_len,
+        args.label_len,
+        args.pred_len,
+        args.d_model,
+        args.n_heads,
+        args.e_layers,
+        args.d_layers,
+        args.d_ff,
+        args.expand,
+        args.d_conv,
+        args.factor,
+        args.embed,
+        args.distil,
+        args.des,
+        args.train_epochs,
+        args.root_path.split("/")[-2],
+        args.target,
+        # TODO args.add_fredf,
+    )
+    
+    # 模型训练
+    if args.is_training: 
+        for ii in range(args.iters):
+            # setting record of experiments
+            training_setting = setting + str(ii)
+            logger.info(f">>>>>>>>> start training: iter-{ii}: {training_setting}>>>>>>>>>>")
+            logger.info(f"{180 * '='}")
+            # 实例化
+            exp = Exp_Forecast(args)
+            # 模型训练
+            model, train_results = exp.train(training_setting)
+            # 模型测试
+            if args.is_testing:
+                logger.info(f">>>>>>>>> start testing: iter-{ii}: {training_setting}>>>>>>>>>>")
+                logger.info(f"{180 * '='}")
+                exp.test(flag="test", setting=training_setting, load=False)
+
+
+
+
+
+# 测试代码 main 函数
+def main():
+    # 设置随机数
+    set_seed(seed = 2023)
+    # 参数解析
+    args = args_parse()
+    print_args(args)
+    # 参数使用
+    run(args)
+
+if __name__ == "__main__":
+    main()
