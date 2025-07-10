94eb5d51a2e6bd9cf2ad4c321ee6b0a378244d2d
wangzhefeng <wangzhefengr@163.com>
2025-07-10 15:08:10 +0800

    update

--- plot_losses.py
+++ plot_losses.py
@@ -27,11 +27,10 @@ import torch
 LOGGING_LABEL = Path(__file__).name[:-3]
 
 
-def plot_values_classifier(train_epochs, examples_seen, 
-                           train_values, val_values, 
-                           label: str = "loss", results_path: str = None):
+def plot_values_classifier(train_epochs, examples_seen, train_values, val_values, label: str = "loss", results_path: str = None):
     # epochs tensor
     epochs_tensor = torch.linspace(0, train_epochs, len(train_values))
+    
     # plot training and validation loss against epochs
     fig, ax1 = plt.subplots(figsize = (5, 3))
     ax1.plot(epochs_tensor, train_values, label = f"Training {label}")
@@ -53,20 +52,46 @@ def plot_values_classifier(train_epochs, examples_seen,
     # grid
     plt.grid(True)
     # save fig
-    plt.savefig(os.path.join(results_path, f"{label}-plot.pdf"))
-    # show fig
+    plt.savefig(Path(results_path).joinpath(f"{label}-plot.pdf"))
+    # show fig on notebook
     plt.show();
 
 
-def plot_losses(train_epochs, 
-                train_losses, vali_losses, 
-                label: str = "loss", results_path: str = None):
+def plot_losses_llm(train_epochs, tokens_seen, train_losses, valid_losses, label: str = "loss", results_path: str = None):
     # epochs seen
     epochs_seen = torch.linspace(0, train_epochs, len(train_losses))
     # plot training and validation loss against epochs
     fig, ax1 = plt.subplots(figsize = (5, 3))
     ax1.plot(epochs_seen, train_losses, label = f"Training {label}")
-    ax1.plot(epochs_seen, vali_losses, label = f"Validation {label}", linestyle = "-.")
+    ax1.plot(epochs_seen, valid_losses, label = f"Validation {label}", linestyle = "-.")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend(loc = "upper right")
+    # only show integer labels on x-axis
+    ax1.xaxis.set_major_locator(MaxNLocator(integer = True))
+    
+    # create a second x-axis for tokens seen
+    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
+    ax2.plot(tokens_seen, train_losses, alpha = 0)  # Invisible plot for aligning ticks
+    ax2.set_xlabel("Tokens seen")
+    
+    # adjust layout to make room
+    fig.tight_layout()
+    # grid
+    plt.grid(True)
+    # save fig
+    plt.savefig(Path(results_path).joinpath(f"{label}_plot.pdf"))
+    # show fig
+    plt.show();
+
+
+def plot_losses(train_epochs, train_losses, valid_losses, label: str = "loss", results_path: str = None):
+    # epochs seen
+    epochs_seen = torch.linspace(0, train_epochs, len(train_losses))
+    # plot training and validation loss against epochs
+    fig, ax1 = plt.subplots(figsize = (5, 3))
+    ax1.plot(epochs_seen, train_losses, label = f"Training {label}")
+    ax1.plot(epochs_seen, valid_losses, label = f"Validation {label}", linestyle = "-.")
     ax1.set_xlabel("Epochs")
     ax1.set_ylabel(label.capitalize())
     ax1.legend(loc = "upper right")
@@ -77,9 +102,10 @@ def plot_losses(train_epochs,
     # grid
     plt.grid(True)
     # save fig
-    plt.savefig(os.path.join(results_path, f"{label}_plot.pdf"))
+    plt.savefig(Path(results_path).joinpath(f"{label}_plot.pdf"))
     # show fig
     plt.show();
+
 
 
 
