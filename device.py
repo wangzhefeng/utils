c6e3f1d1202c3c5fe98ef3acaecd98503d894d96
wangzhefeng <wangzhefengr@163.com>
2025-08-24 03:20:56 +0800

    update

--- device.py
+++ device.py
@@ -23,6 +23,7 @@ from pathlib import Path
 ROOT = str(Path.cwd())
 if ROOT not in sys.path:
     sys.path.append(ROOT)
+from importlib.metadata import version
 
 import torch
 
@@ -58,6 +59,7 @@ def device_setting(verbose: bool = False):
     if verbose:
         # logger.info(f"{'Using device':<13}: {device.type.upper():<13}")
         logger.info(f"{'Using device':<13}: {str(device):<13}")
+        logger.info(f"{'torch version':<13}: {version('torch'):<13}")
         logger.info(f"{40 * '='}")
 
     return device
