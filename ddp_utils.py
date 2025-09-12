7c400af350db5827669f73c9f4e9d04fe19f8348
wangzhefeng <wangzhefengr@163.com>
2025-09-13 04:22:24 +0800

    update

--- ddp_utils.py
+++ ddp_utils.py
@@ -20,12 +20,10 @@ from pathlib import Path
 ROOT = str(Path.cwd())
 if ROOT not in sys.path:
     sys.path.append(ROOT)
-import random
 import platform
 import warnings
 warnings.filterwarnings("ignore")
 
-import numpy as np
 import torch
 import torch.multiprocessing as mp
 import torch.distributed as dist
@@ -37,33 +35,17 @@ from utils.log_util import logger
 LOGGING_LABEL = Path(__file__).name[:-3]
 
 
-def seed_torch(seed: int=0):
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-
-
-def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2**32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
-
-
 def is_dist():
     return dist.is_available() and dist.is_initialized()
 
 
-def init_dist():
-    dist.init_process_group(backend="nccl")
-    torch.cuda.set_device(dist.get_rank())
+# def init_dist():
+#     if is_dist():
+#         dist.init_process_group(backend="nccl")
 
 
-def init_env_dist():
-    dist.init_process_group(backend="gloo|nccl")
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
-
-
-def ddp_setup(rank: int, world_size: int):
+# TODO
+def init_dist(rank, world_size):
     """
     function to initialize a distributed process group(1 process/GPU)
     this allows communication among processes
@@ -72,16 +54,23 @@ def ddp_setup(rank: int, world_size: int):
         rank (int): a unique process ID(Unique identifier of each process)
         world_size (int): total number of processes in the group
     """
+    logger.info(f"init dist...")
     # Only set MASTER_ADDR and MASTER_PORT if not already defined by torchrun
     # rank of machine running rank:0 process, assume all GPUs are on the same machine
     if "MASTER_ADDR" not in os.environ:
         os.environ["MASTER_ADDR"] = "localhost"
-    
+    logger.info(f"os.environ['MASTER_ADDR']: {os.environ['MASTER_ADDR']}")
+
     # any free port on the machine
     if "MASTER_PORT" not in os.environ:
-        os.environ["MASTER_PORT"] = "12345"
+        os.environ["MASTER_PORT"] = "12355"
+    logger.info(f"os.environ['MASTER_PORT']: {os.environ['MASTER_PORT']}")
     
     # initialize process group
+    # rank = get_global_rank()  # 整个分布式集群中的全局进程编号
+    # logger.info(f"rank: {rank}")
+    # world_size = get_world_size()
+    # logger.info(f"world_size: {world_size}")
     if platform.system() == "Windows":
         # Disable libuv because PyTorch for Windows isn't built with support
         os.environ["USE_LIBUV"] = "0"
@@ -91,111 +80,124 @@ def ddp_setup(rank: int, world_size: int):
     else:
         # nccl: NVIDIA Collective Communication Library
         dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    logger.info(f"os.environ['USE_LIBUV']: {os.environ['USE_LIBUV']}")
     # set deivce
     torch.cuda.set_device(rank)
 
 
 def destory_dist():
+    logger.info(f"destory dist...")
+    # Only set MASTER_ADDR and MASTER_PORT if not already defined by torchrun
     dist.destroy_process_group()
 
 
 def is_master():
-    return dist.get_rank() == 0 if is_dist() else True
+    return get_global_rank() == 0 if is_dist() else True
+
+
+def get_global_rank():
+    """
+    全局 Rank: 整个分布式集群中的全局进程编号
+    
+    torch.distributed.get_rank() —— 全局 Rank（RANK）
+        - 定义：在整个分布式训练集群中，所有进程的唯一编号。
+        - 范围：从 0 到 world_size - 1，跨机器、跨节点。
+        - 来源：由 torch.distributed.init_process_group() 自动分配，基于启动命令（如 torchrun）。
+        - 用途：
+            - 用于全局通信（如 all_reduce, broadcast）
+            - 决定哪个进程保存模型（通常只让 rank == 0 保存）
+            - 控制数据采样器（DistributedSampler）如何切分数据
+    """
+    if "RANK" in os.environ:
+        rank = int(os.environ["RANK"])
+    elif torch.distributed.is_initialized():
+        rank = dist.get_rank()
+    else:
+        rank = 0
+
+    return rank
 
 
 def get_world_size():
-    return dist.get_world_size() if is_dist() else 1
-
-
-def get_rank():
-    return dist.get_rank() if is_dist() else 0
-
-
-def get_env_world_size():
     if "WORLD_SIZE" in os.environ:
         world_size = int(os.environ["WORLD_SIZE"])
+    elif torch.distributed.is_initialized():
+        world_size = dist.get_world_size()
     else:
-        # world_size = 1
-        world_size = torch.cuda.device_count()
+        world_size = 1
+        # world_size = torch.cuda.device_count()
 
     return world_size
 
 
-def get_env_rank(): 
-    if "LOCAL_RANK" in os.environ:
-        rank = int(os.environ["LOCAL_RANK"])
-    elif "RANK" in os.environ:
-        rank = int(os.environ["RANK"])
+def get_local_rank():
+    """
+    当前节点（机器）内的进程编号
+        - 定义：当前单台机器（节点）内部的进程编号。
+        - 范围：从 0 到 (nproc_per_node - 1)，仅限于本机。
+        -来源：由 torchrun 或 torch.distributed.launch 自动设置为环境变量：`os.environ["LOCAL_RANK"]`
+        - 用途：
+            - 绑定当前进程到本机的某一块 GPU（配合 CUDA_VISIBLE_DEVICES）
+            - 设置设备：device = torch.device(f'cuda:{local_rank}')
+            - 避免多个进程争抢同一块 GPU
+    """
+    if is_dist() and "LOCAL_RANK" in os.environ:
+        local_rank = int(os.environ["LOCAL_RANK"])
     else:
-        rank = 0
-    
-    return rank
+        local_rank = 0
+
+    return local_rank
 
 
 def dist_gather(o):
-    o_all = [None for _ in range(dist.get_world_size())]
+    o_all = [None for _ in range(get_world_size())]
     dist.all_gather_object(o_all, o)
 
     return o_all
 
 
 def wrap_model(model):
-    model = DDP(model, device_ids=[dist.get_rank()])
+    model = DDP(model, device_ids=[get_global_rank()])
 
     return model
 
 
-def print_master_log():
-    if is_master():
+def print_master_log(rank: int=0):
+    if rank == 0:
         # PyTorch version
         logger.info(f"PyTorch version: {torch.__version__}")
-        # CUDA version
+        # CUDA version and CUDA capability
         if torch.cuda.is_available():
+            # CUDA version
             logger.info(f"CUDA version: {torch.version.cuda}")
-        # CUDA capability
-        capability = torch.cuda.get_device_capability()
-        if capability[0] >= 7:
-            torch.set_float32_matmul_precision("high")
-            logger.info("Uses tensor cores.")
-        else:
-            logger.info("Tensor cores not supported on this GPU. Using default precision.")
+            # CUDA capability
+            capability = torch.cuda.get_device_capability()
+            if capability[0] >= 7:
+                torch.set_float32_matmul_precision("high")
+                logger.info("Uses tensor cores.")
+            else:
+                logger.info("Tensor cores not supported on this GPU. Using default precision.")
 
 
 
 
 # 测试代码 main 函数
 def main():
-    ddp_setup(rank=0, world_size=1)
-    destory_dist()
+    init_dist()
+    logger.info(dist.get_rank())
+
+    # logger.info(f"is_dist: {is_dist()}")
+    # init_dist()
+    # destory_dist()
+    # logger.info(f"is_master: {is_master()}")
+
+    # world_size = get_world_size()
+    # logger.info(f"world_size: {world_size}")
+
+    # rank = get_global_rank()
+    # logger.info(f"rank: {rank}")
     
-    if is_dist():
-        init_dist()
-        init_env_dist()
-        ddp_setup(rank=0, world_size=1)
-        destory_dist()
-
-    
-    logger.info(f"is_dist: {is_dist()}")
-
-    world_size = get_world_size()
-    logger.info(f"world_size: {world_size}")
-
-    world_env_size = get_env_world_size()
-    logger.info(f"world_env_size: {world_env_size}")
-
-    rank = get_rank()
-    logger.info(f"rank: {rank}")
-
-    env_rank = get_env_rank()
-    logger.info(f"env_rank: {env_rank}")
-
-
-    IS_master = is_master()
-    logger.info(f"is_master: {IS_master}")
-    print_master_log()
-
-    # device = torch.device("cuda", rank)
-    # logger.info(f"device: {device}, device.index: {device.index}")
+    # print_master_log()
 
 if __name__ == "__main__":
     main()
